dba421b829b5bee38234837ba07baa6520fb4e49
Hiroaki Nakamura <hnakamur@gmail.com>
2022-10-15 01:41:18 +0900

    Copy loopback example

--- main.go
+++ main.go
@@ -1,104 +1,127 @@
 package main
 
 import (
-	"context"
+	"flag"
+	"fmt"
 	"log"
 	"os"
 	"os/signal"
-	"path/filepath"
-	"strings"
+	"path"
+	"runtime/pprof"
 	"syscall"
+	"time"
 
 	"github.com/hanwen/go-fuse/v2/fs"
-	"github.com/hanwen/go-fuse/v2/fuse"
 )
 
-// files contains the files we will expose as a file system
-var files = map[string]string{
-	"file":              "content",
-	"subdir/other-file": "other-content",
-}
+func writeMemProfile(fn string, sigs <-chan os.Signal) {
+	i := 0
+	for range sigs {
+		fn := fmt.Sprintf("%s-%d.memprof", fn, i)
+		i++
 
-// inMemoryFS is the root of the tree
-type inMemoryFS struct {
-	fs.Inode
-}
-
-// Ensure that we implement NodeOnAdder
-var _ = (fs.NodeOnAdder)((*inMemoryFS)(nil))
-
-// OnAdd is called on mounting the file system. Use it to populate
-// the file system tree.
-func (root *inMemoryFS) OnAdd(ctx context.Context) {
-	for name, content := range files {
-		dir, base := filepath.Split(name)
-
-		p := &root.Inode
-
-		// Add directories leading up to the file.
-		for _, component := range strings.Split(dir, "/") {
-			if len(component) == 0 {
-				continue
-			}
-			ch := p.GetChild(component)
-			if ch == nil {
-				// Create a directory
-				ch = p.NewPersistentInode(ctx, &fs.Inode{},
-					fs.StableAttr{Mode: syscall.S_IFDIR})
-				// Add it
-				p.AddChild(component, ch, true)
-			}
-
-			p = ch
+		log.Printf("Writing mem profile to %s\n", fn)
+		f, err := os.Create(fn)
+		if err != nil {
+			log.Printf("Create: %v", err)
+			continue
 		}
-
-		// Make a file out of the content bytes. This type
-		// provides the open/read/flush methods.
-		embedder := &fs.MemRegularFile{
-			Data: []byte(content),
+		pprof.WriteHeapProfile(f)
+		if err := f.Close(); err != nil {
+			log.Printf("close %v", err)
 		}
-
-		// Create the file. The Inode must be persistent,
-		// because its life time is not under control of the
-		// kernel.
-		child := p.NewPersistentInode(ctx, embedder, fs.StableAttr{})
-
-		// And add it
-		p.AddChild(base, child, true)
 	}
 }
 
-// This demonstrates how to build a file system in memory. The
-// read/write logic for the file is provided by the MemRegularFile type.
 func main() {
-	// This is where we'll mount the FS
-	mntDir, _ := os.CreateTemp("", "")
-	defer func() {
-		if err := os.Remove(mntDir.Name()); err != nil {
-			log.Printf("failed to delete temporary mount directory %s: %s", mntDir.Name(), err)
+	log.SetFlags(log.Lmicroseconds)
+	// Scans the arg list and sets up flags
+	debug := flag.Bool("debug", false, "print debugging messages.")
+	other := flag.Bool("allow-other", false, "mount with -o allowother.")
+	quiet := flag.Bool("q", false, "quiet")
+	ro := flag.Bool("ro", false, "mount read-only")
+	cpuprofile := flag.String("cpuprofile", "", "write cpu profile to this file")
+	memprofile := flag.String("memprofile", "", "write memory profile to this file")
+	flag.Parse()
+	if flag.NArg() < 2 {
+		fmt.Printf("usage: %s ORIGINAL MOUNTPOINT\n", path.Base(os.Args[0]))
+		fmt.Printf("\noptions:\n")
+		flag.PrintDefaults()
+		os.Exit(2)
+	}
+	if *cpuprofile != "" {
+		if !*quiet {
+			fmt.Printf("Writing cpu profile to %s\n", *cpuprofile)
 		}
-	}()
-
-	root := &inMemoryFS{}
-	server, err := fs.Mount(mntDir.Name(), root, &fs.Options{
-		MountOptions: fuse.MountOptions{Debug: false},
-	})
-	if err != nil {
-		log.Panic(err)
+		f, err := os.Create(*cpuprofile)
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(3)
+		}
+		pprof.StartCPUProfile(f)
+		defer pprof.StopCPUProfile()
+	}
+	if *memprofile != "" {
+		if !*quiet {
+			log.Printf("send SIGUSR1 to %d to dump memory profile", os.Getpid())
+		}
+		profSig := make(chan os.Signal, 1)
+		signal.Notify(profSig, syscall.SIGUSR1)
+		go writeMemProfile(*memprofile, profSig)
+	}
+	if *cpuprofile != "" || *memprofile != "" {
+		if !*quiet {
+			fmt.Printf("Note: You must unmount gracefully, otherwise the profile file(s) will stay empty!\n")
+		}
 	}
 
-	log.Printf("Mounted on %s", mntDir.Name())
+	orig := flag.Arg(0)
+	loopbackRoot, err := fs.NewLoopbackRoot(orig)
+	if err != nil {
+		log.Fatalf("NewLoopbackRoot(%s): %v\n", orig, err)
+	}
+
+	sec := time.Second
+	opts := &fs.Options{
+		// These options are to be compatible with libfuse defaults,
+		// making benchmarking easier.
+		AttrTimeout:  &sec,
+		EntryTimeout: &sec,
+	}
+	opts.Debug = *debug
+	opts.AllowOther = *other
+	if opts.AllowOther {
+		// Make the kernel check file permissions for us
+		opts.MountOptions.Options = append(opts.MountOptions.Options, "default_permissions")
+	}
+	if *ro {
+		opts.MountOptions.Options = append(opts.MountOptions.Options, "ro")
+	}
+	// First column in "df -T": original dir
+	opts.MountOptions.Options = append(opts.MountOptions.Options, "fsname="+orig)
+	// Second column in "df -T" will be shown as "fuse." + Name
+	opts.MountOptions.Name = "loopback"
+	// Leave file permissions on "000" files as-is
+	opts.NullPermissions = true
+	// Enable diagnostics logging
+	if !*quiet {
+		opts.Logger = log.New(os.Stderr, "", 0)
+	}
+	server, err := fs.Mount(flag.Arg(1), loopbackRoot, opts)
+	if err != nil {
+		log.Fatalf("Mount fail: %v\n", err)
+	}
+	if !*quiet {
+		fmt.Println("Mounted!")
+	}
 
 	c := make(chan os.Signal, 1)
-	signal.Notify(c)
+	signal.Notify(c, os.Interrupt)
 	go func() {
 		s := <-c
 		log.Printf("Got signal: %s, unmounting and exiting", s)
 		server.Unmount()
 	}()
 
-	log.Print("Unmount and exit by pressing Ctrl-C")
-
-	// Wait until unmount before exiting
 	server.Wait()
 }
