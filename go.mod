b0ca8d35a0359fb1f3d5b0eddfafa074188ec737
Hiroaki Nakamura <hnakamur@gmail.com>
2022-10-15 15:42:20 +0900

    (WIP)

--- go.mod
+++ go.mod
@@ -2,6 +2,17 @@ module github.com/hnakamur/agefuse
 
 go 1.19
 
-require github.com/hanwen/go-fuse/v2 v2.1.0
+require (
+	filippo.io/age v1.0.0
+	github.com/hanwen/go-fuse/v2 v2.1.0
+	github.com/hnakamur/ageutil v0.0.1
+)
 
-require golang.org/x/sys v0.0.0-20180830151530-49385e6e1522 // indirect
+require (
+	filippo.io/edwards25519 v1.0.0-rc.1 // indirect
+	go.uber.org/atomic v1.7.0 // indirect
+	go.uber.org/multierr v1.8.0 // indirect
+	golang.org/x/crypto v0.0.0-20221010152910-d6f0a8c073c2 // indirect
+	golang.org/x/sys v0.0.0-20210903071746-97244b99971b // indirect
+	golang.org/x/term v0.0.0-20220919170432-7a66f970e087 // indirect
+)
--- main.go
+++ main.go
@@ -1,18 +1,132 @@
 package main
 
 import (
+	"context"
 	"flag"
 	"fmt"
 	"log"
 	"os"
 	"os/signal"
 	"path"
+	"path/filepath"
 	"runtime/pprof"
 	"syscall"
 	"time"
+	"unsafe"
 
+	"filippo.io/age"
 	"github.com/hanwen/go-fuse/v2/fs"
+	"github.com/hanwen/go-fuse/v2/fuse"
+	"github.com/hnakamur/ageutil"
 )
+
+type AgeFSRoot struct {
+	fs.LoopbackRoot
+	identities []age.Identity
+}
+
+type AgeFSNode struct {
+	fs.LoopbackNode
+}
+
+var _ = (fs.NodeStatfser)((*AgeFSNode)(nil))
+var _ = (fs.NodeStatfser)((*AgeFSNode)(nil))
+var _ = (fs.NodeGetattrer)((*AgeFSNode)(nil))
+var _ = (fs.NodeGetxattrer)((*AgeFSNode)(nil))
+var _ = (fs.NodeSetxattrer)((*AgeFSNode)(nil))
+var _ = (fs.NodeRemovexattrer)((*AgeFSNode)(nil))
+var _ = (fs.NodeListxattrer)((*AgeFSNode)(nil))
+var _ = (fs.NodeReadlinker)((*AgeFSNode)(nil))
+var _ = (fs.NodeOpener)((*AgeFSNode)(nil))
+var _ = (fs.NodeCopyFileRanger)((*AgeFSNode)(nil))
+var _ = (fs.NodeLookuper)((*AgeFSNode)(nil))
+var _ = (fs.NodeOpendirer)((*AgeFSNode)(nil))
+var _ = (fs.NodeReaddirer)((*AgeFSNode)(nil))
+var _ = (fs.NodeMkdirer)((*AgeFSNode)(nil))
+var _ = (fs.NodeMknoder)((*AgeFSNode)(nil))
+var _ = (fs.NodeLinker)((*AgeFSNode)(nil))
+var _ = (fs.NodeSymlinker)((*AgeFSNode)(nil))
+var _ = (fs.NodeUnlinker)((*AgeFSNode)(nil))
+var _ = (fs.NodeRmdirer)((*AgeFSNode)(nil))
+var _ = (fs.NodeRenamer)((*AgeFSNode)(nil))
+
+type AgeFSFile struct {
+	fs.FileHandle
+	// buf *bytes.Buffer
+}
+
+var _ = (fs.FileHandle)((*AgeFSFile)(nil))
+
+// var _ = (fs.FileReleaser)((*AgeFSFile)(nil))
+// var _ = (fs.FileGetattrer)((*AgeFSFile)(nil))
+var _ = (fs.FileReader)((*AgeFSFile)(nil))
+
+// var _ = (fs.FileWriter)((*AgeFSFile)(nil))
+// var _ = (fs.FileGetlker)((*AgeFSFile)(nil))
+// var _ = (fs.FileSetlker)((*AgeFSFile)(nil))
+// var _ = (fs.FileSetlkwer)((*AgeFSFile)(nil))
+// var _ = (fs.FileLseeker)((*AgeFSFile)(nil))
+// var _ = (fs.FileFlusher)((*AgeFSFile)(nil))
+// var _ = (fs.FileFsyncer)((*AgeFSFile)(nil))
+// var _ = (fs.FileSetattrer)((*AgeFSFile)(nil))
+// var _ = (fs.FileAllocater)((*AgeFSFile)(nil))
+
+func NewAgeFSRoot(rootPath string, identities []age.Identity) (fs.InodeEmbedder, error) {
+	var st syscall.Stat_t
+	err := syscall.Stat(rootPath, &st)
+	if err != nil {
+		return nil, err
+	}
+
+	root := &AgeFSRoot{
+		LoopbackRoot: fs.LoopbackRoot{
+			Path: rootPath,
+			Dev:  uint64(st.Dev),
+			NewNode: func(rootData *fs.LoopbackRoot, parent *fs.Inode, name string, st *syscall.Stat_t) fs.InodeEmbedder {
+				return &AgeFSNode{
+					LoopbackNode: fs.LoopbackNode{
+						RootData: rootData,
+					},
+				}
+			},
+		},
+		identities: identities,
+	}
+
+	return root.NewNode(&root.LoopbackRoot, nil, "", &st), nil
+}
+
+func (n *AgeFSNode) AgeFSRoot() *AgeFSRoot {
+	return (*AgeFSRoot)(unsafe.Pointer(n.RootData))
+}
+
+func (n *AgeFSNode) Open(ctx context.Context, flags uint32) (fh fs.FileHandle, fuseFlags uint32, errno syscall.Errno) {
+	flags = flags &^ syscall.O_APPEND
+	p := n.path()
+	f, err := syscall.Open(p, int(flags), 0)
+	if err != nil {
+		return nil, 0, fs.ToErrno(err)
+	}
+	lf := NewAgeFSFile(f)
+	return lf, 0, 0
+}
+
+// path returns the full path to the file in the underlying file
+// system.
+func (n *AgeFSNode) path() string {
+	path := n.Path(n.Root())
+	return filepath.Join(n.RootData.Path, path)
+}
+
+func NewAgeFSFile(fd int) *AgeFSFile {
+	return &AgeFSFile{
+		FileHandle: fs.NewLoopbackFile(fd),
+	}
+}
+
+func (f *AgeFSFile) Read(ctx context.Context, buf []byte, off int64) (res fuse.ReadResult, errno syscall.Errno) {
+	return f.FileHandle.(fs.FileReader).Read(ctx, buf, off)
+}
 
 func writeMemProfile(fn string, sigs <-chan os.Signal) {
 	i := 0
@@ -42,11 +156,16 @@ func main() {
 	ro := flag.Bool("ro", false, "mount read-only")
 	cpuprofile := flag.String("cpuprofile", "", "write cpu profile to this file")
 	memprofile := flag.String("memprofile", "", "write memory profile to this file")
+	privName := flag.String("priv", "", "private key filename")
 	flag.Parse()
 	if flag.NArg() < 2 {
 		fmt.Printf("usage: %s ORIGINAL MOUNTPOINT\n", path.Base(os.Args[0]))
 		fmt.Printf("\noptions:\n")
 		flag.PrintDefaults()
+		os.Exit(2)
+	}
+	if *privName == "" {
+		fmt.Printf("private key filename must be set with -priv flag.")
 		os.Exit(2)
 	}
 	if *cpuprofile != "" {
@@ -75,8 +194,15 @@ func main() {
 		}
 	}
 
+	identities, err := ageutil.ParseSSHPrivateKeyFile(*privName)
+	if err != nil {
+		fmt.Printf("failed to load private key: %s", err)
+		os.Exit(1)
+	}
+	log.Printf("identities=%+v", identities)
+
 	orig := flag.Arg(0)
-	loopbackRoot, err := fs.NewLoopbackRoot(orig)
+	loopbackRoot, err := NewAgeFSRoot(orig, identities)
 	if err != nil {
 		log.Fatalf("NewLoopbackRoot(%s): %v\n", orig, err)
 	}
